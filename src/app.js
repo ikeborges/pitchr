1e119b3d684072eca348e21416b9a348e8e0cfb2
Kaique Borges <rique.kaique@gmail.com>
2022-04-19 00:29:32 -0300

    Create basic dynamic feed

--- src/app.js
+++ src/app.js
@@ -7,7 +7,22 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get("/", (req, res) => {
-  res.render('index', {message: "Hello world"})
+  const decks = [{
+    id: 1,
+    title: "WeFund",
+    author: "Kaique Borges",
+    uploadedAt: "Dec 15, 2022",
+    coverUrl: "https://ucarecdn.com/30f6ddbb-73e1-4c33-ae67-9fa139a49e03/-/format/auto/-/quality/smart_retina/-/preview/"
+  },
+  {
+    id: 2,
+    title: "WeFund",
+    author: "Kaique Borges",
+    uploadedAt: "Dec 15, 2022",
+    coverUrl: "https://ucarecdn.com/30f6ddbb-73e1-4c33-ae67-9fa139a49e03/-/format/auto/-/quality/smart_retina/-/preview/"
+    }]
+
+  res.render('index', { decks })
 })
 
 app.get("/upload-deck", (req, res) => {
